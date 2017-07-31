655e988ebbdb9040914811aa6a3b78a79486acec
Brian Clark <bclark@corp.epnet.com>
2017-07-31 06:11:18 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine', 'hbs');
+
+
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`;
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if (err) {
+            console.log('unable to log');
+        }
+    });
+    next();
+})
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear()
+});
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+        res.render('home.hbs', {
+          pageTitle: 'Header Home Page',
+          welcomeMessage: 'Welcome to Node!'
+    });
+});
+
+app.get('/about', (req, res) => {
+        res.render('about.hbs', {
+          pageTitle: 'New About Page'
+    });
+});
+
+app.get('/bad', (req, res) => {
+        res.send({
+        error: '400',
+        message: 'Bad request'
+    });
+});
+
+app.listen(3000);
